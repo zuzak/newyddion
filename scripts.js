937b452dd9bd52db8f68cc7292fb9485cba86b39
Douglas Gardner <douglas.gardner@digital.homeoffice.gov.uk>
2019-07-02 15:12:23 +0100

    +

--- scripts.js
+++ scripts.js
@@ -24,11 +24,9 @@ function wget (url, cb) {
 }
 
 var BBC_NEWS_URL = 'https://polling.bbc.co.uk/news/latest_breaking_news_waf'
-var GDN_NEWS_URL = 'https://api.nextgen.guardianapps.co.uk/news-alert/alerts'
 var REU_NEWS_URL = 'https://files.chippy.ch/newsboard/reuters.php' // proxy of http://uk.reuters.com/assets/breakingNews?view=json
 var BBC_LOCAL_URL = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.bbci.co.uk%2Fnews%2Fengland%2Fsouth_yorkshire%2Frss.xml'
 var REUTERS_WIRE_URL = 'https://files.chippy.ch/newsboard/reuterswire.php' // proxy of https://uk.reuters.com/assets/jsonWireNews#'
-var BLOOMBERG_URL = 'https://files.chippy.ch/newsboard/bloomberg.php' // https://www.bloomberg.com/api/modules/id/europe_breaking_news
 var MI5_URL = 'https://api.rss2json.com/v1/api.json?rss_url=https://www.mi5.gov.uk/UKThreatLevel/UKThreatLevel.xml'
 var output = {}
 
@@ -58,44 +56,6 @@ function sendNews (provider, header, headline, description) {
 }
 
 function poll () {
-  wget('https://files.chippy.ch/newsboard/travis.json', function (err, repos) {
-    if (!err) {
-      for (var i = 0; i < repos.length; i++) {
-        wget('https://api.travis-ci.org/repos/' + repos[i] + '/branches/master', function (err, response) {
-          if (!err) {
-            var state = response.branch.state
-            if (state !== 'passed') {
-              var str = [
-                response.commit.message,
-                null,
-                response.commit.author_name + ' on ' + response.commit.branch,
-                '<span class="' + state + '">Build ' + state + '</span>'
-              ].join('<br>')
-              sendNews('travis', 'Removals Wallboard #' + response.branch.number, str)
-            } else {
-              sendNews('travis')
-            }
-          }
-        })
-      }
-    }
-  })
-  wget(MI5_URL, function (err, response) {
-    if (!err && response.status === 'ok') {
-      var str = response.items[0].title
-      var level = str.substr(str.search(/[A-Z]+$/))
-      var div = document.querySelector('.threatlevel')
-
-      if (level !== 'SEVERE') {
-        div.innerHTML = 'Current Threat Level <span>' + level + '</span>'
-      } else {
-        wget('https://files.chippy.ch/newsboard/teaspoons.php', function (er, res) {
-          if (er) return
-          div.innerHTML = 'Teaspoons as of ' + res.date + '<span>' + res.count + '</span>'
-        })
-      }
-    }
-  })
   wget(BBC_NEWS_URL, function (err, response) {
     if (err) {
       sendNews('bbc')
@@ -108,31 +68,6 @@ function poll () {
       }
     }
   })
-  /* wget('http://localhost:3000', function (err, response) {
-    console.log('AAA', err, response)
-    if (!err) {
-      var res = []
-
-      for (var i = 0; i < response.length; i++) {
-        if (response[i] === 'ok') res.push('<span class="green">&#x2B23;</span>') // black hexagon
-        if (response[i] === 'stale') res.push('<span class="yellow">&#x2B20;</span>') // white hexagon
-        if (response[i] === 'old') res.push('<span class="red">&#x2B1F;</span>') // black pentagon
-      }
-      console.log(res, response, 'foo')
-      var div = document.querySelector('.local')
-      div.innerHTML = res.join(' ')
-    }
-  }) */
-  /* wget(GDN_NEWS_URL, function (err, response) {
-    if (!err) {
-      var story = response.collections[0].content[0]
-      if (story) {
-        sendNews('gdn', story.title, story.headline)
-      } else {
-        sendNews('gdn')
-      }
-    }
-  }) */
   wget(REU_NEWS_URL, function (err, story) {
     if (err) {
       sendNews('reu')
@@ -253,21 +188,9 @@ function poll () {
       sendNews('reu-wire')
     }
     sendNews('reu-wire')
-  })
-  wget(BLOOMBERG_URL, function (err, data) {
-    if (!err && data && data.items && data.items.length > 0) {
-      var story = data.items[0]
-      sendNews('bloomberg', story.editorialTitle, story.headline, story.type)
-    } else {
-      sendNews('bloomberg')
-    }
-  })
+  });
   // sendNews('bbc-local', 'Sheffield', 'Foo bar baz')
-  if (!navigator.onLine) {
-    sendNews('placeholder', 'Offline!', 'Waiting for network connection...')
-  } else {
-    sendNews('placeholder')
-  }
+
 }
 
 function displayNews () {
@@ -302,12 +225,20 @@ const clockTick = function () {
   }
   var body = document.getElementById('js-body')
   var screenSaverClass = 'screenSaver'
+  var placeholderClass = 'offline'
   if (now.getHours() % 2) {
     if (body.className.indexOf(screenSaverClass) === -1) {
       body.className += ' ' + screenSaverClass
     }
   } else {
     document.getElementById('js-body').className.replace(' ' + screenSaverClass, '')
+  }
+  if (!navigator.onLine) {
+    if (body.className.indexOf(placeholderClass) === -1) {
+      body.className += ' ' + placeholderClass
+    }
+  } else {
+    document.getElementById('js-body').className.replace(' ' + placeholderClass, '')
   }
 }
 
